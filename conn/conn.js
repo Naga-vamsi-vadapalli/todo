ed93cec12b289ffd0a06e9dff91b248d1c2a9771
Naga Vamsi <nagavamsivadapalli99@gmail.com>
2024-11-13 16:59:09 +0530

    first commit

--- conn/conn.js
+++ conn/conn.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
+require('dotenv').config(); // Make sure to load environment variables
 
-const conn = async (req, res) => {
+const conn = async () => {
   try {
-    await mongoose
-      .connect(
-        ""
-      )
-      .then(() => {
-        console.log("Connected");
-      });
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
   } catch (error) {
-    console.log(error);
+    console.log("Error connecting to MongoDB", error);
   }
 };
+
 conn();
